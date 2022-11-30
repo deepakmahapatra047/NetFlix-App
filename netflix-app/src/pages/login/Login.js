4aa126bd4ce936ea6c1b7300f4b561928a0a85bd
Bharat Singh <you@example.com>
2022-11-30 18:52:16 +0530

    initial Commit

---
+++ netflix-app/src/pages/login/Login.js
@@ -0,0 +1,25 @@
+import "./login.css";
+
+function Login() {
+    return (
+        <div className="login">
+            <div className="container">
+                <form>
+                    <h1>Sign In</h1>
+                    <input type="email" placeholder="Email or phone number" />
+                    <input type="password" placeholder="Password" />
+                    <button className="loginButton">Sign In</button>
+                    <span>
+                        New to Netflix? <b>Sign up now.</b>
+                    </span>
+                    <small>
+                        This page is protected by Google reCAPTCHA to ensure you're not a
+                        bot. <b>Learn more</b>.
+                    </small>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
